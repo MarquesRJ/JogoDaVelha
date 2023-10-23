9663637fed04b964f281d5c3fcfcddd41db5ccb5
Gabriel Marques <61622466+MarquesRJ@users.noreply.github.com>
2023-10-23 16:18:24 -0300

    finalização

--- src/Main.java
+++ src/Main.java
@@ -59,6 +59,33 @@ public class Main {
                 System.out.println();
             }
 
+            //verifica se tem algum ganhador
+            if ((Velha [0][0] == 'X' && Velha[0][1] == 'X' && Velha [0][2] == 'X') || //linha 1
+                (Velha [1][0] == 'X' && Velha[1][1] == 'X' && Velha [1][2] == 'X') || //linha 2
+                (Velha [2][0] == 'X' && Velha[2][1] == 'X' && Velha [2][2] == 'X') || //linha 3
+                
+                (Velha [0][0] == 'X' && Velha[1][0] == 'X' && Velha [2][0] == 'X') || //coluna 1
+                (Velha [0][1] == 'X' && Velha[1][1] == 'X' && Velha [2][1] == 'X') || //coluna 2
+                (Velha [0][2] == 'X' && Velha[1][2] == 'X' && Velha [2][2] == 'X') || //coluna 3
+                //diagonal
+                (Velha [0][0] == 'X' && Velha[1][1] == 'X' && Velha [2][2] == 'X')) {
+            ganhou = true;
+            System.out.println("Parabéns, o jogador 1 ganhou!");
+                } else if ((Velha [0][0] == 'O' && Velha[0][1] == 'O' && Velha [0][2] == 'O') || //linha 1
+                (Velha [1][0] == 'O' && Velha[1][1] == 'O' && Velha [1][2] == 'O') || //linha 2
+                (Velha [2][0] == 'O' && Velha[2][1] == 'O' && Velha [2][2] == 'O') || //linha 3
+                
+                (Velha [0][0] == 'O' && Velha[1][0] == 'O' && Velha [2][0] == 'O') || //coluna 1
+                (Velha [0][1] == 'O' && Velha[1][1] == 'o' && Velha [2][1] == 'O') || //coluna 2
+                (Velha [0][2] == 'O' && Velha[1][2] == 'O' && Velha [2][2] == 'O') || //coluna 3
+                //diagonal
+                (Velha [0][0] == 'O' && Velha[1][1] == 'O' && Velha [2][2] == 'O')) {
+            ganhou = true;
+            System.out.println("Parabéns, o jogador 2 ganhou!");
+                } else if (jogada > 9) {
+                    ganhou = true;
+                    System.out.println("Ninguém ganhou a partida!")
+                }
         }
     }
 }
