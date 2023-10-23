03eb7d5d4e1505d08ca83305bdb00bc92e88306d
MarquesRJ <gabrielmarques142@gmail.com>
2023-10-23 15:29:50 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        char[][] Velha = new char[3][3];
+
+        System.out.println("Jogador 1 = X");
+        System.out.println("Jogador 2 = O");
+
+        boolean ganhou = false;
+        int jogada = 1;
+        char sinal;
+        int linha = 0, coluna = 0;
+
+        while (!ganhou) {
+
+            if (jogada % 2 == 1) { // jogador 1
+                System.out.println("Vez do jogador 1. Escolha uma linha e coluna (1-3).");
+                sinal = 'X';
+            } else {
+                System.out.println("Vez do jogador 2. Escolha uma linha e coluna (1-3).");
+                sinal = 'O';
+            }
+
+            boolean linhaValida = false;
+            while (!linhaValida) {
+                System.out.println("Entre com a linha (1, 2 ou 3).");
+                linha = scan.nextInt();
+                if (linha >= 1 && linha <= 3) {
+                    linhaValida = true;
+                } else {
+                    System.out.println("Entrada inválida, tente novamente.");
+                }
+            }
+            boolean colunaValida = false;
+            while (!colunaValida) {
+                System.out.println("Entre com a coluna (1, 2, ou 3)");
+                coluna = scan.nextInt();
+                if (coluna >= 1 && coluna <= 3) {
+                    colunaValida = true;
+                } else {
+                    System.out.println("Entrada inválida, tente novamente.");
+                }
+            }
+            linha--;
+            coluna--;
+            if (Velha[linha][coluna] == 'X' || Velha[linha][coluna] == 'O') {
+                System.out.println("Posição já utilizada, tente novamente.");
+            } else { // jogada válida
+                Velha[linha][coluna] = sinal;
+                jogada++;
+            }
+            // imprimir tabuleiro
+            for (int i = 0; i < Velha.length; i++) {
+                for (int j = 0; j < Velha[i].length; j++) {
+                    System.out.println(Velha[i][j] + " | ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+}
